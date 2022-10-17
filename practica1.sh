a5a9382a6a27a67909c617f9bcb1b369be746d50
Lucia <1668160@uab.cat>
2022-10-17 11:19:27 +0200

    començament pas5

--- practica1.sh
+++ practica1.sh
@@ -46,8 +46,8 @@ awk -F',' '{if ($12 != ""){print $0}}' Movies.csv > Movies_columna12.csv
 awk -F',' '{if ($13 != ""){print $0}}' Movies_columna12.csv > Movies_columna13.csv
 awk -F',' '{if ($14 != ""){print $0}}' Movies_columna13.csv > Movies_columna14.csv
 awk -F',' '{if ($15 != ""){print $0}}' Movies_columna14.csv > Movies_columna15.csv
-var9=$(wc -l < Movies_columna12.csv)
-# S'han esborrat 404 línies del Movies.csv
+var9=$(wc -l < Movies_columna15.csv)
+# S'han esborrat 549 línies del Movies.csv
 var10=$(( $var7-$var9 ))
 echo "S'han esborrat $var10 linies de Movies.csv"
 
@@ -55,7 +55,19 @@ awk -F',' '{if ($12 != ""){print $0}}' Shows.csv > Shows_columna12.csv
 awk -F',' '{if ($13 != ""){print $0}}' Shows_columna12.csv > Shows_columna13.csv
 awk -F',' '{if ($14 != ""){print $0}}' Shows_columna13.csv > Shows_columna14.csv
 awk -F',' '{if ($15 != ""){print $0}}' Shows_columna14.csv > Shows_columna15.csv
-var11=$(wc -l < Shows_columna12.csv)
-# S'han esborrat 231 línies del Shows.csv
+var11=$(wc -l < Shows_columna15.csv)
+# S'han esborrat 316 línies del Shows.csv
 var12=$(( $var6-$var11 ))
 echo "S'han esborrat $var12 linies de Shows.csv"
+
+
+# PAS 5
+# vamos a la primera línia i que guarde el valor de la columna 13 en una variable
+# vaya a la siguiente linia y compare el valor de la variable con el valor de la columnaa 13
+# si el valor de la linia es más grande que el de la variable, que guarde ese nuevo valor en la variable; sino que no haga nad
+# HAY QUE HACERLO CON LA COLUMNA 13 Y LA 14
+awk -F',' {}
+
+# queremos que añada en la última columna de cada fila lo que pone entre "$13/variable creada antes"
+# HAY QUE HACERLO CON EL MOVIES Y EL SHOWS
+awk '{print $0 "\t" ", $($13/$(variablecreada1), $($14/$(variablecreada2))"}' Movies_columna15.csv > Movies_columna16.csv
