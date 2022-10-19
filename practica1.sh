65e1d7addf59213ece62bf6b1068b5343fb83f1a
Lucia <1668160@uab.cat>
2022-10-19 08:32:57 +0200

    correcció shell script: if

--- practica1.sh
+++ practica1.sh
@@ -5,35 +5,27 @@ var1=$(wc -l < titles.cvs)
 grep ^t"*[ms]" titles.cvs > pas1
 var2=$(wc -l < pas1)
 var3=$(( $var1-$var2 ))
-# S'han esborrat 171 línes
 echo "S'han esborrat $var3 linies"
-
+# S'han esborrat 171 línes
 
 
 # PAS 2
-# Busca a la segona columna els caràcters [#'"¿¡] i la resta, els guarda en pas2
 grep -v "^[[:alnum:]]*,[#\'\"\¿\¡]" pas1 > pas2
 var4=$(wc -l < pas2)
 var5=$(( $var2-$var4 ))
-# S'han esborrat 12 línies
 echo "S'han esborrat $var5 linies"
+# S'han esborrat 11 línies
 
 
 # PAS 3
-# Busca les línies que comencen per ts (show) i les guarda a Shows.csv
-# Contem quantes línies hi ha a l'arxiu
 grep  "^ts" pas2 > Shows.csv
 var6=$(wc -l < Shows.csv)
-# Busca les líines que comencen per tm (movie) i les guarda a Movies.csv
+# A l'arxiu Shows.csv hi ha 2045 línies
 grep "^tm" pas2 > Movies.csv
 var7=$(wc -l < Movies.csv)
-
+# A l'arxiu Movies.csv hi ha 3749 línies
 var8=$(( $var6+$var7 ))
-
-echo $var8
-echo $var4
-
-#hay que hacer un if: ESTÁ MAL
+# La suma de les línies dels dos arxius és 5794
 if [ $var8 == $var4 ]; then
   echo "1"
 else
